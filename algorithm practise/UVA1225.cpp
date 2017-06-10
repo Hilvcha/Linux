badc811f1b9ffa1837620c91ab6dfbc776114866
hilvcha <micah_wang@163.com>
2017-06-11 00:44:11 +0800

    1586

---
+++ algorithm practise/UVA1225.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+//#include<map>
+//#include<iomanip>
+//#include<cstdio>
+#include<cstring>
+#include<cctype>
+#include<typeinfo>
+using namespace std;
+int a[10]={0};
+int deal(int num){
+    if(num<10){
+        a[num]++;
+        return 0;
+    }
+    a[num%10]++;
+    num/=10;
+    return deal(num);
+}
+int main(){
+   freopen("in","r",stdin);
+
+    double sum=0;
+    int flag=0,num_value=0,n=0;
+    cin>>n;
+    cin.ignore();
+    while(n--){
+        cin>>num_value;
+        cin.ignore();;
+        for (int i = 1; i <= num_value; i++){
+            deal(i);
+        }
+        for(int i=0;i<10;i++){
+            cout<<a[i]<<' ';
+        }
+        cout<<endl;
+        memset(a,0,sizeof(a));
+    }
+    return 0;
+}
